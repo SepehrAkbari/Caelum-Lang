9b602d0f67b71027ef32ef9dc6ca4085e9d9cf9d
Sepehr Akbari <isepehrakbari@gmail.com>
2025-02-25 23:57:10 -0600

    Update Main.hs

--- CaelumLang/app/Main.hs
+++ CaelumLang/app/Main.hs
@@ -2,5 +2,17 @@ module Main (main) where
 
 import Lib
 
+-- My Imports
+import Parser (parseExpr)
+import Evaluator (eval)
+import System.IO (hFlush, stdout)
+
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Enter an arithmetic expression:"
+  input <- getLine
+  case parseExpr input of
+    Left err  -> print err
+    Right ast -> do
+      let result = eval ast
+      putStrLn $ "Result: " ++ show result
